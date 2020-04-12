ee6f6b6e3392d52fa47fcaaf039dd073fe78c51b
Himanshu Negi <himanshunegi3295@gmail.com>
2020-04-12 12:23:00 +0530

    first commit

---
+++ DemoHib/src/main/java/first/DemoHib/Delete.java
@@ -0,0 +1,23 @@
+package first.DemoHib;
+
+import org.hibernate.*;
+
+public class Delete {
+
+	
+	public static void main(String args[])
+	{
+		
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			
+			
+		}catch(Exception e)
+		{
+			
+		}
+		
+	}
+}
